26fc9ee5ea1a78726a44309932f70a613fd73def
Alex P <alex.pantec@gmail.com>
2015-04-08 01:26:32 +0300

    Login

--- src/main/java/alexp/blog/controller/LoginController.java
+++ src/main/java/alexp/blog/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,6 +15,16 @@ public class LoginController {
 
     @Autowired
     private UserService userService;
+
+    @RequestMapping(value = "/login_success", method= RequestMethod.GET)
+    public @ResponseBody String loginSuccess(HttpServletRequest request) {
+        return "ok";
+    }
+
+    @RequestMapping(value = "/login_error", method= RequestMethod.GET)
+    public @ResponseBody String loginError(HttpServletRequest request) {
+        return "failed";
+    }
 
     // POST request must be used for logout if CSRF enabled, so this page contains hidden form to submit via JS
     @RequestMapping(value = "/logout", method= RequestMethod.GET)
