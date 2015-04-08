53b6f098ca503628ce2d31060539dce9f06fe81e
Alex P <alex.pantec@gmail.com>
2015-04-08 10:15:22 +0300

    Remember me

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,3 +11,5 @@ INSERT INTO users_roles(user_id, role_id) VALUES(1, 2);
 INSERT INTO users_roles(user_id, role_id) VALUES(2, 1);
 
 INSERT INTO users_roles(user_id, role_id) VALUES(3, 1);
+
+CREATE TABLE IF NOT EXISTS persistent_logins (username varchar(64) not null, series varchar(64) primary key, token varchar(64) not null, last_used timestamp not null)
