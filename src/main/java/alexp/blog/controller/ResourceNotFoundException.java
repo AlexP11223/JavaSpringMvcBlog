dc03436afdb79b4b61b001cb4e819607850efd48
Alex P <alex.pantec@gmail.com>
2015-04-09 00:23:34 +0300

    ResourceNotFoundException (404)

---
+++ src/main/java/alexp/blog/controller/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package alexp.blog.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+}
